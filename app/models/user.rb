28b42be1bdd6186b593d6489399e72df4fe246d8
viral85 <viral85@gmail.com>
2020-03-11 15:11:58 +0530

    Updates model validation for user and raffle type

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { scope: :raffle_types_id }
 
   def odds_msg(type_count)
     return "Thanks for signing up! Your odds of winning are #{(100/type_count.to_f).round(2)}%."
